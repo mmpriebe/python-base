5d4a7de968f44f383342eceffe0ef9cced934f81
mmpriebe <marciano.priebe@gmail.com>
2024-06-06 23:57:21 -0300

    Melhorando o script

--- hello.py
+++ hello.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python3
+"""Hello World Multi Linguas.
 
-#Este programa imprime hello world
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente.
 
-print("Hello, World") #  test-ignore
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Marciano Priebe"
+__licence__ = "Unlicense"
+
+current_language = "en_US"
+
+msg = "Hello, World"
+
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+
+print(msg)
+
 
 
